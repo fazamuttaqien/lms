6fe0dd4848e579f0954a237ee466c385dddb0604
fazamuttaqien <fazamttqn@gmail.com>
2025-06-25 09:09:16 +0700

    feat(auth): implement login functionality with GitHub and email verification
    - Added LoginForm component for user authentication via GitHub and email.
    - Integrated email OTP verification process.
    - Created VerifyEmail page for handling email verification.
    - Set up authentication client using better-auth with email OTP plugin.
    - Established database schema for User, Session, Account, and Verification models.
    - Updated environment configuration for better-auth and email sending.
    - Enhanced layout with Toaster for notifications.
    - Added necessary dependencies for authentication and email handling.

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from "./db";
+import { env } from './env';
+import { resend } from './resend';
+import { emailOTP } from "better-auth/plugins"
+
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: 'postgresql',
+    }),
+    socialProviders: {
+        github: {
+            clientId: env.AUTH_GITHUB_CLIENT_ID,
+            clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+        }
+    },
+    plugins: [
+        emailOTP({
+            async sendVerificationOTP({ email, otp }) {
+                await resend.emails.send({
+                    from: 'Acme LMS <onboarding@resend.dev>',
+                    to: [email],
+                    subject: 'Acme LMS - Verify your email',
+                    react: `<p>Your OTP is <strong>z${otp}</strong></p>`,
+                })
+            }
+        })
+    ]
+})
