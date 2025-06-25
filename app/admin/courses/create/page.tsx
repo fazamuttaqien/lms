1d8c75b0fa875ae233498d643fbfc629822e61b2
fazamuttaqien <fazamttqn@gmail.com>
2025-06-25 16:55:55 +0700

    feat(file-uploader): implement file upload functionality with drag-and-drop support

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -13,6 +13,7 @@ import slugify from "slugify";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { RichTextEditor } from "@/components/rich-text-editor/Editor";
+import { Uploader } from "@/components/file-uploader/Uploader";
 
 export default function CourseCreatePage() {
     const form = useForm<CourseSchemaType>({
@@ -136,9 +137,7 @@ export default function CourseCreatePage() {
                                     <FormItem className="w-full">
                                         <FormLabel>Thumbnail Image</FormLabel>
                                         <FormControl>
-                                            <Input
-                                                placeholder="Thumbnail URL"
-                                                {...field} />
+                                            <Uploader />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
---
+++ app/api/s3/upload/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { z } from "zod"
+
+export const fileUploadSchema = z.object({
+    fileName: z.string().min(1, { message: "Filename is required" }),
+    contentType: z.string().min(1, { message: "Content type is required" }),
+    size: z.string().min(1, { message: "Size is required" }),
+    isImage: z.boolean(),
+})
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+
+        const validation = fileUploadSchema.safeParse(body);
+        if (!validation.success) {
+            return NextResponse.json(
+                { error: "Invalid Request Body" },
+                { status: 400 },
+            )
+        }
+
+        
+    } catch (error) {
+
+    }
+}
